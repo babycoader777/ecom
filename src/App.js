f0f6c4df9b4494bbdef248a4dbd7766c9d50c26d
babycoader777 <01fe20bcs181@kletech.ac.in>
2023-08-05 16:11:35 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import {Button} from 'react-bootstrap';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>E-com Dashboard</h1>
+     
+      <button>button</button>
+      <Button>Button</Button>
+    </div>
+  );
+}
+
+export default App;
